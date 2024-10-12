d78aefe4848a709c7a64fc4f8e03233abb58ca45
Jordi Carrera Ventura <jordi.carrera.ventura@gmail.com>
2024-10-12 12:52:13 +0200

    Add tests for `ExactStringMatcher`

--- src/snippyts/vocabulary_tools.py
+++ src/snippyts/vocabulary_tools.py
@@ -122,9 +122,64 @@ class FuzzyStringMatcher(StringMatcher):
 
 
 
+def test_exact_add_words():
+
+    terms = ["uno", "dos", "tres"]
+    sm = ExactStringMatcher(case_sensitive=True)
+    sm.add("uno")
+    sm.add("dos")
+    sm.add("tres")
+
+    assert not sm("Uno")
+    assert sm("uno")
+    assert len(sm("uno")) == 1
+    assert len(sm("Uno")) == 0
+
+
+def test_exact_iadd_words():
+
+    terms = ["uno", "dos", "tres"]
+    sm = ExactStringMatcher(case_sensitive=True)
+    sm += terms
+
+    assert not sm("Uno")
+    assert sm("uno")
+    assert len(sm("uno")) == 1
+    assert len(sm("Uno")) == 0
+
+
+def test_exact_contains_word():
+
+    terms = ["uno", "dos", "tres"]
+    sm = ExactStringMatcher(case_sensitive=True)
+    sm += terms
+
+    assert "uno" in sm
+    assert "tres" in sm
+    assert "Uno" not in sm
+    assert "cuatro" not in sm
+
+
+def test_exact_filter_words():
+
+    terms = ["uno", "dos", "tres"]
+    sm = ExactStringMatcher(case_sensitive=True)
+    sm += terms
+
+    words = ["dos", "cinco", "tres", "cuatro"]
+    expected = [True, False, True, False]
+
+    filtered = sm.filter(words)
+    assert filtered == expected
+
+
 
 def test():
 
+    test_exact_add_words()
+    test_exact_iadd_words()
+    test_exact_contains_word()
+    test_exact_filter_words()
     terms = [
        "orca", "Orco", "orco", "oro",
        "orwelliano", "oráculo", "oración",
